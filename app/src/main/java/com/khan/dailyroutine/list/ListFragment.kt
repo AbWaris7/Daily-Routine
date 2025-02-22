784d71ca5f1c18ee5cf5e9da0dd21d65bb4d0515
AbWaris7 <hellokhan.ch@gmail.com>
2025-02-22 12:43:56 +0100

    move fragments to seperate folders

--- app/src/main/java/com/khan/dailyroutine/list/ListFragment.kt
+++ app/src/main/java/com/khan/dailyroutine/list/ListFragment.kt
@@ -1,10 +1,11 @@
-package com.khan.dailyroutine
+package com.khan.dailyroutine.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.khan.dailyroutine.R
 
 class ListFragment : Fragment() {
 
